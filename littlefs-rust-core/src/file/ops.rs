cdf6a43715278f796cf63fd2a6750c370bc914c9
taks <857tn859@gmail.com>
2026-08-08 23:53:25 +0900

    fix

--- littlefs-rust-core/src/file/ops.rs
+++ littlefs-rust-core/src/file/ops.rs
@@ -446,7 +446,7 @@ pub fn lfs_file_close_(lfs: &mut crate::fs::Lfs, file: &mut LfsFile) -> Result<(
         lfs_mlist_remove(lfs, ::core::mem::transmute(::core::ptr::from_mut(file)));
 
         let cfg = file.cfg;
-        if !cfg.is_null() && (*cfg).buffer.is_empty() {
+        if !cfg.is_null() && (&(*cfg).buffer).is_empty() {
             #[cfg(feature = "alloc")]
             {
                 crate::lfs_alloc_module::lfs_free(
