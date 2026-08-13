e464d2235dc3be6344e41c7a5cd06e319e187132
taks <857tn859@gmail.com>
2026-08-14 00:16:45 +0900

    Revert "compact when dir count hits 0x3ff"
    This reverts commit 5bceccfebb266124f06fee95eee5f89b287ff3e5.

--- littlefs-rust-core/src/dir/commit.rs
+++ littlefs-rust-core/src/dir/commit.rs
@@ -1643,10 +1643,6 @@ fn lfs_dir_commit_size_raw(
 ///         lfs->gdisk = lfs->gstate;
 ///         lfs->gdelta = (lfs_gstate_t){0};
 ///
-///         if(dir->count == 0x3ff)
-///         {
-///             goto compact;
-///         }
 ///         goto fixmlist;
 ///     }
 ///
@@ -1853,9 +1849,6 @@ pub fn lfs_dir_relocatingcommit(
                                 tag: 0,
                                 pair: [0, 0],
                             };
-                            if (dir.count == 0x3ff) {
-                                do_compact = true;
-                            }
                         }
                     }
                 }
