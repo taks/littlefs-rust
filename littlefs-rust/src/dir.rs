c32449d76c1c776ce4c9c976e83404b6b2120ec2
taks <857tn859@gmail.com>
2026-09-11 20:52:14 +0900

    wip

--- littlefs-rust/src/dir.rs
+++ littlefs-rust/src/dir.rs
@@ -97,7 +97,7 @@ pub(crate) fn dir_entry_from_info(info: &LfsInfo) -> DirEntry {
         .position(|&b| b == 0)
         .unwrap_or(info.name.len());
     let name = core::str::from_utf8(&info.name[..nul]).unwrap_or("").into();
-    let file_type = if info.type_ == littlefs_rust_core::lfs_type::lfs_type::LFS_TYPE_DIR {
+    let file_type = if info.type_ == littlefs_rust_core::lfs_type::LfsType::DIR {
         FileType::Dir
     } else {
         FileType::File
